d23bdcd7e4c4dfb8f2f37a2594ec12784aa0200f
Elena Tsypileva <etsypileva@gmail.com>
2020-10-26 11:26:36 +0300

    improved SomeTest.java

--- src/test/java/ru/netology/postman/SomeTest.java
+++ src/test/java/ru/netology/postman/SomeTest.java
@@ -15,7 +15,7 @@ public class SomeTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("data"))
+                .body("data",equalTo("some data"))
                 ;
     }
 }
