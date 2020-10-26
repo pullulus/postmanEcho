62061f38b904ba5722dd19cf2d0cbe3855bad2d3
Elena Tsypileva <etsypileva@gmail.com>
2020-10-26 11:17:19 +0300

    added SomeTest.java

---
+++ src/test/java/ru/netology/postman/SomeTest.java
@@ -0,0 +1,21 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SomeTest {
+    @Test
+    void shouldPostSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("some data"))
+                ;
+    }
+}
